48ad71bf2b038b47f5f76c4be263f6171cc558e4
leonan2 <leonan2@hotmail.com>
2016-07-19 15:42:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,33 +2,41 @@
 
 $botToken = "262810724:AAELbOXZZext7xBrQoOXT7WJWdJQLUFwPjk";
 
-$website = "https://api.telegram.org/bot".$botToken; 
-
-$update = file_get_contents($website."/getupdates");
-
+$website = "https://api.telegram.org/bot".$botToken;
+ 
+$update = file_get_contents('php://input');
 $update = json_decode($update, TRUE);
-
-$chatId = $update ["message"]["chat"]["id"];
-$message = $update ["message"]["text"];
-
-switch ($message)
- {
-    case "/test":
-      sendMessage($chatId, "test");
-      break;
-    case "/hi":
-      sendMessage($chatId, "hey there");
-      break;
-    default: 
-      sendMessage($chatId, "default");
- }
-function sendMessage ($chatId, $message) {
-  $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text=".urlencode($message);
-  file_get_contents($url);
-
-  
-  
+ 
+ 
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+ 
+ 
+switch($message) {
+       
+        case "/test":
+                sendMessage($chatId, "test");
+                break;
+        case "/hi":
+                sendMessage($chatId, "hi there!");
+                break;
+        default:
+                sendMessage($chatId, "default");
+       
 }
+ 
+function sendMessage ($chatId, $message) {
+       
+        $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text=".urlencode($message);
+        file_get_contents($url);
+       
+}
+ 
+ 
+ 
+ 
+ 
+?>
 
 /*
 $text = end($updateArray["result"])["message"]["text"];
@@ -41,4 +49,4 @@ print_r($text);
 
 file_get_contents($website."/sendmessage?chat_id=".$id."&text=".$text)
 */
-?>
+
