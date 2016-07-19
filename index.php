34111a779dd384cc59416af88b4421a315d69a7a
leonan2 <leonan2@hotmail.com>
2016-07-19 15:25:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 
 $botToken = "262810724:AAELbOXZZext7xBrQoOXT7WJWdJQLUFwPjk";
-$website = "https://api.telegram.org/bot".$botToken;
 
+$website = "https://telegrambotleonan.herokuapp.com/";
+/*$website = "https://api.telegram.org/bot".$botToken; */
 $update = file_get_contents($website."/getupdates");
 
 $updateArray = json_decode($update, TRUE);
