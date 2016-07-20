ca35d22fdff2c0df5fa37397cbb391b4519047d4
leonan2 <leonan2@hotmail.com>
2016-07-20 12:19:06 -0300

    Update botcommands.php

--- botcommands.php
+++ botcommands.php
@@ -1,30 +1,40 @@
 <?php
-
-$botToken = "262810724:AAELbOXZZext7xBrQoOXT7WJWdJQLUFwPjk";
-$website = "https://api.telegram.org/bot".$botToken;
-
-$update = file_get_contents("php://input");
-$update= json_decode($update, TRUE);
-
-$chatId = $update["message"]["chat"]["id"];
-$message = $update["message"]["text"];
-
-switch($message) {
-       
-        case "/test":
-                sendMessage($chatId, "test");
-                break;
-        case "/hi":
-                sendMessage($chatId, "hi there!");
-                break;
-        default:
-                sendMessage($chatId, "default");
-       
-}
- 
-function sendMessage ($chatId, $message) {
-       
-        $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text=".urlencode($message);
-        file_get_contents($url);
-       
+require 'vendor/autoload.php';
+use Telegram\Bot\Api;
+$telegram = new Api('263074817:AAGLTJYhzahHybKzyUzFVFEuk8LpxkZkjqc');
+/*
+$response = $telegram->getMe();
+$botId = $response->getId();
+$firstName = $response->getFirstName();
+$username = $response->getUsername();
+echo $response;
+*/
+//$updates = $telegram->getWebhookUpdates();
+//$response = $telegram->getUpdates();
+//$updateArray = json_decode($updates, TRUE);
+//$chatId = $updateArray["result"][0]["message"]["chat"]["id"];
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+$texto = $update['message']['text'];
+$chatId = $update['message']['chat']['id'];
+if ($update) {
+  switch ($update['message']['text']) {
+    case 'oi':
+      $response = $telegram->sendMessage([
+        'chat_id' => $chatId,
+        'text' => 'Oi, tudo bem?'
+      ]);
+      $response = $telegram->sendMessage([
+        'chat_id' => $chatId,
+        'text' => 'qr tc?'
+      ]);
+      break;
+    default:
+      $response = $telegram->sendMessage([
+        'chat_id' => $chatId,
+        'text' => 'texto invalido, apenas oi está disponivel'
+      ]);
+      
+      break;
+  }
 }
