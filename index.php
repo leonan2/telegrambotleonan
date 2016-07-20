54cda4f0a86c364472427343b9874711c8fbd362
leonan2 <leonan2@hotmail.com>
2016-07-20 11:43:07 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,5 +9,27 @@ $updateArray = json_decode($update, TRUE);
 
 $chatId = $updateArray["result"][0]["message"]["chat"]["id"];
 
+if ($update) {
+  switch ($update['message']['text']) {
+    case 'oi':
+      $response = $telegram->sendMessage([
+        'chat_id' => $chatId,
+        'text' => 'Oi, tudo bem?'
+      ]);
+      $response = $telegram->sendMessage([
+        'chat_id' => $chatId,
+        'text' => 'qr tc?'
+      ]);
+      break;
+    default:
+      $response = $telegram->sendMessage([
+        'chat_id' => $chatId,
+        'text' => 'texto invalido, apenas oi está disponivel'
+      ]);
+      
+      break;
+  }
+}
+
 file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=test");
 print_r($updateArray);
