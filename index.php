f7f15c4b6332d14cf6cdb15bf2fc96f8ae9fb1aa
leonan2 <leonan2@hotmail.com>
2016-07-20 10:39:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ switch($message) {
     
 }
 
-function sendMesasge ($chatId, $message) {
+function sendMessage ($chatId, $message) {
   $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text=".urlencode($message);
   file_get_contents($url);
 }
