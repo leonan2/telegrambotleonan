a06bfebd16cb0ea8f4b03226b8df25ab0b18f2b8
leonan2 <leonan2@hotmail.com>
2016-07-20 10:50:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,38 @@
 <?php
-
-ini_set('error_reporting' , E_ALL);
-
+ 
 $botToken = "262810724:AAELbOXZZext7xBrQoOXT7WJWdJQLUFwPjk";
 $website = "https://api.telegram.org/bot".$botToken;
-
+ 
 $update = file_get_contents('php://input');
 $update = json_decode($update, TRUE);
-
+ 
+ 
 $chatId = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
-
+ 
+ 
 switch($message) {
-  
-  case "/test":
-    sendMessage($chatId, "text");
-    break;
-  case "/hi":
-    sendMessage($chatId, "hi");
-    break;
-  default:
-    sendMessage($chatId, "default");
-    
+       
+        case "/test":
+                sendMessage($chatId, "test");
+                break;
+        case "/hi":
+                sendMessage($chatId, "hi there!");
+                break;
+        default:
+                sendMessage($chatId, "default");
+       
 }
-
+ 
 function sendMessage ($chatId, $message) {
-  $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text=".urlencode($message);
-  file_get_contents($url);
+       
+        $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text=".urlencode($message);
+        file_get_contents($url);
+       
 }
-
+ 
+ 
+ 
+ 
+ 
 ?>
-
-<h2> Test feito </h2>
