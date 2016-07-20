fabd9f2c61949a335db94ed985f9d411fc8dd744
leonan2 <leonan2@hotmail.com>
2016-07-20 13:40:34 -0300

    Update enviararq.php

--- enviararq.php
+++ enviararq.php
@@ -7,7 +7,7 @@ $website = "https://api.telegram.org/bot".$botToken;
 
 <form action="<?php echo $website.'/sendPhoto' ?>" method="post" enctype="multipart/form-data">
 
-  <input type="text" name="chat_id" value="31057258" />
+  <input type="text" name="chat_id" value="239528113" />
   <input type="file" name="photo" />
   <input type="submit" value="Enviar" />
 
