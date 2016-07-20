d910815f91eabf3da02900fb335061590ad993f0
leonan2 <leonan2@hotmail.com>
2016-07-20 13:23:17 -0300

    Update and rename botleonan.php to index.php

--- index.php
+++ index.php
@@ -24,14 +24,6 @@ switch($message) {
 	
 }
 
-function sendMessage ($chatId, $message) {
-	
-	$url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text=".urlencode($message);
-	file_get_contents($url);
-	
-}
- 
-
 
 
 
