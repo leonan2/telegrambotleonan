77d563b6300a0ad5b60d805c0c6de385f7d47a02
leonan2 <leonan2@hotmail.com>
2016-07-20 13:37:07 -0300

    Create enviararq.php

---
+++ enviararq.php
@@ -0,0 +1,14 @@
+<?php
+
+$botToken = "262810724:AAF4sjm5EgN6WZvvueFu3SCvx8G65kKAIyE";
+$website = "https://api.telegram.org/bot".$botToken;
+
+?>
+
+<form action="<?php echo $website.'/sendPhoto' ?>" method="post" enctype="multipart/form-data">
+
+  <input type="text" name="chat_id" value="31057258" />
+  <input type="file" name="photo" />
+  <input type="submit" value="Enviar" />
+
+</form>
