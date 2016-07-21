d09c0a5adbbd960544fdb8bc69d47626ee2bbb19
leonan2 <leonan2@hotmail.com>
2016-07-21 15:11:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $botToken = "262810724:AAF4sjm5EgN6WZvvueFu3SCvx8G65kKAIyE";
 $website = "https://api.telegram.org/bot".$botToken;
 
-$update = file_get_contents('php://input);
+$update = file_get_contents('php://input');
 
 $updateArray = json_decode($update, TRUE);
 
