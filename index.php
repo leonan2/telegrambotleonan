482a1fc03e047ebada167af692506392e199c1fc
leonan2 <leonan2@hotmail.com>
2016-07-21 15:06:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,32 +3,11 @@
 $botToken = "262810724:AAF4sjm5EgN6WZvvueFu3SCvx8G65kKAIyE";
 $website = "https://api.telegram.org/bot".$botToken;
 
-$update = file_get_contents('php://input');
-$update = json_decode($update, TRUE);
+$update = file_get_contents($website."/getupdates");
 
+$updateArray = json_decode($update, TRUE);
 
-$chatId = $update["message"]["chat"]["id"];
-$message = $update["message"]["text"];
+$chatId = $updateArray["result"][0]["message"]["chat"]["id"];
 
-
-switch($message) {
-	
-	case "/test":
-		sendMessage($chatId, "test");
-		break;
-	case "/hi":
-		sendMessage($chatId, "hi there!");
-		break;
-	default: 
-		sendMessage($chatId, "default");
-	
-}
-
-function sendMessage ($chatId, $message) {
-	
-	$url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text=".urlencode($message);
-	file_get_contents($url);
-	
-}
- 
-
+file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=test");
+print_r($updateArray);
