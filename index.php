f34e139806e9fcfdbea746cde2e9ef2fa4c0b37e
leonan2 <leonan2@hotmail.com>
2016-07-22 13:01:19 -0300

    Update index.php

--- index.php
+++ index.php
@@ -20,16 +20,33 @@ $texto =  $update['message']['text'];
 $texto = strtolower($texto);
 $chatId = $update['message']['chat']['id'];
 
-
-
-
-
 if ($texto) {
   switch ($texto) {
     case '/start':
       $response = $telegram->sendMessage([
         'chat_id' => $chatId,
         'text' => 'Oi, tudo bem? / (responda com tudo e você?)'
+        $keyboard = [
+                     ['7', '8', '9'],
+                     ['4', '5', '6'],
+                     ['1', '2', '3'],
+                          ['0']
+        ];
+
+        $reply_markup = $telegram->replyKeyboardMarkup([
+          'keyboard' => $keyboard, 
+          'resize_keyboard' => true, 
+          'one_time_keyboard' => true
+        ]);
+
+          $response = $telegram->sendMessage([
+              'chat_id' => 'CHAT_ID', 
+              'text' => 'Hello World', 
+              'reply_markup' => $reply_markup
+          ]);
+          
+          $messageId = $response->getMessageId();
+        
       ]);
       break;
     case 'oi':
