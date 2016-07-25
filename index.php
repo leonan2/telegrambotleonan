2dc4c2cf352a46e42d1ae8eae21fbdeb8abea860
leonan2 <leonan2@hotmail.com>
2016-07-25 12:04:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -64,6 +64,12 @@ if ($texto) {
         'text' => 'bye!'
       ]);
       break;
+      case 'idiota':
+      $response = $telegram->sendMessage([
+        'chat_id' => $chatId,
+        'text' => 'olha quem fala, a pessoa que está aqui conversando com um robô'
+      ]);
+      break;
      default:
       $response = $telegram->sendMessage([
         'chat_id' => $chatId,
