e42981039d5ec624453589c33f057f0aa2bd28dc
leonan2 <leonan2@hotmail.com>
2016-07-25 11:59:31 -0300

    Update index.php

--- index.php
+++ index.php
@@ -26,26 +26,6 @@ if ($texto) {
       $response = $telegram->sendMessage([
         'chat_id' => $chatId,
         'text' => 'Oi, tudo bem? / (responda com tudo e você?)'
-          $keyboard = [
-                    ['7', '8', '9'],
-                    ['4', '5', '6'],
-                    ['1', '2', '3'],
-                         ['0']
-       ];
-
-       $reply_markup = $telegram->replyKeyboardMarkup([
-         'keyboard' => $keyboard,
-         'resize_keyboard' => true,
-         'one_time_keyboard' => true
-       ]);
-
-         $response = $telegram->sendMessage([
-             'chat_id' => 'CHAT_ID',
-             'text' => 'Hello World',
-             'reply_markup' => $reply_markup
-         ]);
-
-         $messageId = $response->getMessageId();
        ]);
       break;
     case 'oi':
